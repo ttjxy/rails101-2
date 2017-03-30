f10feaaa954550e5991ee4bbd149b7c688e06ee9
xy <ttjxy@gmail.com>
2017-03-30 13:13:32 +0800

    xiugai submit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330023042) do
+ActiveRecord::Schema.define(version: 20170330030502) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
