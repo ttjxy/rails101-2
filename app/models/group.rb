bb22069d26dc4ab3c9457b7048dad3314c93845f
xy <ttjxy@gmail.com>
2017-03-30 20:56:06 +0800

    implement group relationship

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,6 @@ class Group < ApplicationRecord
   has_many :posts
   validates :title, presence: true
   scope :recent, -> {order("created_at DESC")}
+  has_many :group_relationships
+  has_many :member, through: :group_relationships, source: :user
 end
