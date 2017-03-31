a1a291f2ddbad3878c9c8e283ac2b8b246782b29
xy <ttjxy@gmail.com>
2017-03-31 12:43:04 +0800

    implement post edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,11 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
@@ -18,10 +23,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to group_path(params[:group_id])
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
-  
+
 end
